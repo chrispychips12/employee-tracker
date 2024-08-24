7f575416765f0d280725feb5ca11e42892eca153
Christopher <94519893+chrispychips12@users.noreply.github.com>
2024-08-24 13:15:35 +1000

    added readme

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,3 +6,4 @@ INSERT INTO role (title, salary, department_id) VALUES ('Salesperson', 50000, 1)
 
 -- Insert an employee
 INSERT INTO employee (first_name, last_name, role_id) VALUES ('John', 'Doe', 1);
+
